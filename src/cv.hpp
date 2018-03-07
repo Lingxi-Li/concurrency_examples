5c04984bf68cff7191942a9061d88ca5030dab29
Lingxi Li <lilingxi.cs@gmail.com>
2018-03-07 16:16:23 +0800

    Simplify condition_variable example

--- src/cv.hpp
+++ src/cv.hpp
@@ -1,5 +1,4 @@
 #include <cassert>
-#include <atomic>
 #include <chrono>
 #include <condition_variable>
 #include <functional>
@@ -10,19 +9,20 @@
 
 namespace cv {
 
+using namespace std::chrono;
+
 std::mutex mut;
 std::condition_variable cond_var;
 std::queue<unsigned> que;
 
-std::atomic_uint counter(0);
-
 void produce(const std::future<void>& start, unsigned loopcnt) {
   start.wait();
   while (loopcnt--) {
+    std::this_thread::sleep_for(50ms);
+    auto item = 1;
     {
-      auto v = counter.fetch_add(1, std::memory_order_relaxed);
       std::lock_guard<std::mutex> lock(mut);
-      que.push(v);
+      que.push(item);
     }
     cond_var.notify_one();
   }
@@ -33,24 +33,23 @@ void consume(const std::future<void>& start) {
   while (true) {
     std::unique_lock<std::mutex> lock(mut);
     cond_var.wait(lock, [] { return !que.empty(); });
-    auto v = que.front();
-    if (v == unsigned(-1)) return;
+    auto item = que.front();
+    if (item == unsigned(-1)) return;
     que.pop();
     lock.unlock();
-    counter.fetch_sub(1, std::memory_order_relaxed);
+    std::this_thread::sleep_for(50ms);
   }
 }
 
 void main() {
-  using namespace std::chrono;
-  constexpr auto loopcnt = 1000000;
+  constexpr auto loopcnt = 100;
   std::promise<void> start_promise;
   auto start_future = start_promise.get_future();
   auto producer_1 = std::async(std::launch::async, produce, std::ref(start_future), loopcnt);
   auto producer_2 = std::async(std::launch::async, produce, std::ref(start_future), loopcnt);
   auto consumer_1 = std::async(std::launch::async, consume, std::ref(start_future));
   auto consumer_2 = std::async(std::launch::async, consume, std::ref(start_future));
-  std::this_thread::sleep_for(2s);
+  std::this_thread::sleep_for(1s);
   start_promise.set_value();
   producer_1.wait();
   producer_2.wait();
@@ -61,7 +60,7 @@ void main() {
   cond_var.notify_all();
   consumer_1.wait();
   consumer_2.wait();
-  assert(counter.load(std::memory_order_relaxed) == 0);
+  assert(que.size() == 1 && que.front() == unsigned(-1));
 }
 
 } // namespace cv
